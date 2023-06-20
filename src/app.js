216e34d18fe6282f9aba12ea68ae54e59cbc1c5d
ArthurKova <metaloutcome@gmail.com>
2023-06-20 12:49:17 +0300

    complete random color btn with interval

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
+import colors from "./data/colors";
+
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+let isActiveColorSwitch = false;
+let colorSwitch;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const randomColor = (colors) => {
+  let min = 0;
+  let max = colors.length - 1;
+  return colors[randomIntegerFromInterval(min, max)];
+};
+
+const bodyColor = (el) => {
+  document.body.style.backgroundColor = randomColor(colors);
+};
+
+startBtn.addEventListener("click", () => {
+  if (isActiveColorSwitch) {
+    return;
+  }
+  colorSwitch = setInterval(() => bodyColor(colors), 1000);
+  isActiveColorSwitch = true;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(colorSwitch);
+  colorSwitch = null;
+  isActiveColorSwitch = false;
+});
